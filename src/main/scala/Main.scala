b95c7f248d8fde6d24255200f3167ed2f73b74cd
Adam Lisner <lisneada@fit.cvut.cz>
2025-12-01 22:22:33 +0100

    AOC day 1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,9 @@ import day01.SecretEntrance
 
 @main def run(): Unit = {
   val solver = new SecretEntrance()
-  val password = solver.solve()
+  val password = solver.solvePart1()
   println(s"The password is: $password")
+  val password2 = solver.solvePart2()
+  println(s"The password for part 2 is: $password2")
 }
 
