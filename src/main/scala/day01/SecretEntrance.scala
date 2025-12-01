6e71edf979f782bc80c507d0073087bd8a75ce72
Adam Lisner <lisneada@fit.cvut.cz>
2025-12-01 22:22:00 +0100

    AOC day 1

--- src/main/scala/day01/SecretEntrance.scala
+++ src/main/scala/day01/SecretEntrance.scala
@@ -1,10 +1,33 @@
 package day01
 
 import scala.io.Source
+import scala.util.Using
 
 class SecretEntrance(val startPos: Int = 50, val ticks: Int = 100) {
 
-  def solve(resourceName: String = "src/main/resources/day01.txt"): Long = {
+  def solvePart1(resourceName: String = "src/main/resources/day01.txt"): Long = {
+    Using(Source.fromFile(resourceName)) { source =>
+      val (_, zeroCount) = source.getLines()
+        .filter(_.nonEmpty)
+        .foldLeft((startPos, 0)) { case ((pos, count), line) =>
+          val cleanLine = line.trim
+          val dirChar = cleanLine.head
+          val dist = cleanLine.tail.trim.toInt
+
+          val nextPos =
+            if (dirChar == 'L') Math.floorMod(pos - dist, ticks)
+            else Math.floorMod(pos + dist, ticks)
+
+          (nextPos, if (nextPos == 0) count + 1 else count)
+        }
+      zeroCount.toLong
+    }.fold(
+      throwable => throw throwable,
+      zeroCount => zeroCount
+    )
+  }
+
+  def solvePart2(resourceName: String = "src/main/resources/day01.txt"): Long = {
     val source = Source.fromFile(resourceName)
     try {
       var pos = startPos
